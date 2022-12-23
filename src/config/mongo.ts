f30dc1b4c656ae909c7b4ba175b9e8afd55182d7
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-12-23 11:22:02 -0300

    modiffied package.json

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,8 +2,11 @@ import 'dotenv/config';
 import { connect } from 'mongoose';
 
 async function dbConnect(): Promise<void> {
-  const DB_URI = <string>process.env.DB_URI;
-  await connect(DB_URI);
+  const { DB_HOST, DB_USER, DB_PASS, DB_DATABASE } = process.env;
+  await connect(
+    // `mongodb://${DB_HOST}/${DB_DATABASE}`
+    `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_DATABASE}?retryWrites=true&w=majority`
+  );
 }
 
 export default dbConnect;
