180c93a430e11e7e1ab3e3cc8a4a0a68856953e2
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-01-02 11:21:18 -0300

    updated CORS config

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,14 @@ import CategoryModel from './models/category';
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-app.use(cors());
+app.use(
+  cors({
+    origin: `*`,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
 app.use(router);
 
